d3c54badd65a72a58dc08826aa4a20f480455c30
Jhonier Barreto <113477481+Garciajs@users.noreply.github.com>
2024-02-26 17:10:35 -0500

    SRC
    archivos de carpeta sources

---
+++ db.config.js
@@ -0,0 +1,9 @@
+//  config//db.config.js
+//conexion a base de datos mysql
+export const dbconfig={
+    host: 'localhost',
+    user: 'root',
+    password: '<12345>',
+    database: 'crudproductos',
+    port: 3308 //solo si el puerto es diferente al predeterminado 3306
+}
